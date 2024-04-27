594090818d2580a1cefed74cde43be58f4123fb4
Gabriela Roa <gabygr1718@gmail.com>
2024-04-26 22:02:08 -0500

    ejercicioenproceso

---
+++ ejercicio5.py
@@ -0,0 +1,12 @@
+nombre_usuario = str(input("Escriba su nombre "))
+sexo_usuario = str(input("Escriba su sexo "))
+
+abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"]
+
+print(nombre_usuario [0])
+
+for i in range(0, 12, 1):
+    print(abecedario [i])
+
+    
+    
