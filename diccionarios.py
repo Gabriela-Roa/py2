880475511424a548f6f44f188153096aaa39b8ec
Gabriela Garcia <gabygr1718@gmail.com>
2024-05-03 21:59:31 -0500

    falta terminar

---
+++ diccionarios.py
@@ -0,0 +1,9 @@
+#ejercicio1
+
+usuario=str(input("Escriba la divisa para mostrar el simbolo correspondiente "))
+
+divisas = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+print(divisas.get(usuario))
+
+#ejercicio2
+
