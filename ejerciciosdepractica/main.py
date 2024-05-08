a8c24b171087c8dca5351e5fdf8bf87af2bf7b80
Gabriela Garcia <gabygr1718@gmail.com>
2024-05-08 16:20:36 -0500

    ejemplo para hacer proyecto

--- ejerciciosdepractica/main.py
+++ ejerciciosdepractica/main.py
@@ -1,40 +1,38 @@
+#imports
 from datos import *
 from menu import *
+from participantes import *
 
-#Constants 
-RUTA_BASE_DE_DATOS ="eventos.json"
+#Constants
+RUTA_BASE_DE_DATOS = "eventos.json"
 
-datos= cargar_datos(RUTA_BASE_DE_DATOS)
+datos = cargar_datos(RUTA_BASE_DE_DATOS)
 
 while True:
     menu_principal()
     opc = pedir_opcion()
     if opc == 1:
-        print("Opcion 1")
+        datos = registrar_participante(datos)
     elif opc == 2:
-        print("Opcion 2")
+        datos = eliminar_participante(datos)
     elif opc == 3:
-        print("Opcion 3")
+        datos = pagar_participante(datos)
     elif opc == 4:
-        print("Opcion 4")
+        print("opcion 4")
     elif opc == 5:
-        print("Opcion 5")
+        print("opcion 5")
     elif opc == 6:
-        print("Opcion 6")
+        print("opcion 6")
     elif opc == 7:
-        print("Opcion 7")
+        cuales_participantes_sin_pagar(datos)
     elif opc == 8:
-        print("Opcion 8")
+        cuantos_participantes_sin_pagar(datos)
     elif opc == 9:
-        print("Opcion 9")
+        print("opcion 9")
     elif opc == 10:
-        print("Opcion 10")
+        participantes_del_mes(datos)
     elif opc == 11:
         print("Salió!!")
-        break 
-    
-datos= cargar_datos(RUTA_BASE_DE_DATOS)
+        break
 
-print(datos)
-   
-guargar_datos(datos, RUTA_BASE_DE_DATOS)
+guardar_datos(datos, RUTA_BASE_DE_DATOS)
